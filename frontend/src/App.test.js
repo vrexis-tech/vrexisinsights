4fc3f871fd8cb2e864d87e34cbd89ec8c81f9a00
vrexis-tech <vrexistech@gmail.com>
2025-06-06 12:51:17 -0500

    Update test to check for Vrexis Insights heading

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Vrexis Insights heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Vrexis Insights/i)).toBeInTheDocument();
 });
